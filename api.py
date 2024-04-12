406a708621917fb8e1a8884346369f7d6162bd0a
Lytzeer <lukasportier@gmail.com>
2024-04-12 10:19:48 +0200

    update api

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from api_func import get_data, get_total_games
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index(data=get_data()):
